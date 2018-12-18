8559cc4d472a3ad05f4010dd851af9235116b38f
kezhi <kezhi@pandu.ie>
2018-12-19 00:50:06 +0800

    so-called functional programming

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -6,17 +6,51 @@ import About from './components/About'
 import Contact from './components/Contact'
 
 class App extends Component {
-  render() {
+  state = {
+    routes:[
+      {path:'/',component:Home},
+      {path:'/about',component:About},
+      {path:'/contact',component:Contact},
+    ]
+  }
+  wrappRouter(components){
     return (
       <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-        </div>
+        {components}
       </BrowserRouter>
-    );
+    )
+  }
+  getRoutes(routes){
+    return routes.map(route => {
+      return (
+         <Route path={route.path || '/'} component={route.component}/>
+      )
+    })
+  }
+  wrapApp(route){
+    return (
+      <div className="App">
+        <Navbar />
+          {route}
+        </div>
+    )
+  }
+  pipe(...fns){
+    return fns.reduce((fna,fnb) => (...args) => fna(fnb(...args)))
+  }
+  render() {
+    const renderMarchine = this.pipe(this.wrappRouter,this.wrapApp,this.getRoutes)
+    return renderMarchine(this.state.routes)
+    // return (
+    //   <BrowserRouter>
+    //     <div className="App">
+    //       <Navbar />
+    //       <Route exact path='/' component={Home}/>
+    //       <Route path='/about' component={About} />
+    //       <Route path='/contact' component={Contact} />
+    //     </div>
+    //   </BrowserRouter>
+    // );
   }
 }
 
